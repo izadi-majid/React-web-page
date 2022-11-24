eeb6b3aa83674888fcc926529e1b9ebf4b5c95fd
majid izadi <izadi.majid73@gmail.com>
2022-11-24 14:07:55 +0330

    reset project and add topbar component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import "./topbar.css";
+import {Search,Person,Chat,Notifications} from "@mui/icons-material";
+
+export default function Topbar() {
+  return (
+    <div className="topbarcontainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input placeholder="search for friend ,post or video" className="searchInput" />
+        </div>
+      </div>
+
+      <div className="topbarRight">
+        <div className="topbarLink">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person className="personIcon" />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat className="chatIcon" />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications className="notifIcon" />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  )
+}
